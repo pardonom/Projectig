81ff10f259241dcec1c2eefc576eca2647a6f625
David <www.grygry@gmail.com>
2025-05-28 20:04:52 +0200

    Looks ok ig

---
+++ Voting.py
@@ -0,0 +1,16 @@
+class Voter:
+    def __init__(self, name, voter_id):
+        self.__name = name
+        self.__voter_id = voter_id
+        self.__already_voted = False
+        print(f"[born]Voter {self.__name}, id: {self.__voter_id}, has been registered.")
+
+    def get_name(self):
+        return self.__name
+    def get_voter_id(self):
+        return self.__voter_id
+
+
+    def __del__(self):
+        print(f"[killed]Voter {self.__name}, id: {self.__voter_id}, has been removed from the system.")
+person1 = Voter("Boron", 1)
