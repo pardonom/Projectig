2116f02a711ef1100fb3fd942029dfb4f44e85a2
David <www.grygry@gmail.com>
2025-05-29 00:18:31 +0200

    IT WORKS!!!!! IT FINALY WORKS

--- Voting.py
+++ Voting.py
@@ -3,18 +3,96 @@ class Voter:
         self.__name = name
         self.__voter_id = voter_id
         self.__already_voted = False
-        print(f"[born]Voter {self.__name}, id: {self.__voter_id}, has been registered.")
+        print(f"[born] Voter {self.__name}, id: {self.__voter_id}, has been registered.")
 
     def get_name(self):
         return self.__name
+
     def get_voter_id(self):
         return self.__voter_id
 
 
-    def __del__(self):
-        print(f"[killed]Voter {self.__name}, id: {self.__voter_id}, has been removed from the system.")
-person1 = Voter("Boron", 1)
+    def vote(self, voting_ballot, candidate, date):
+        if self.__already_voted:
+            print(f" Voter {self.__name}, has already voted.")
 
-class InPersonVoter:
-    def __init__(self, name, voter_id):
-        self.__name = name
+        else:
+            voting_ballot.record_vote(candidate,date)
+            self.__already_voted = True
+
+    def __del__(self):
+        print(f"[killed] Voter {self.__name}, has been removed from the system.")
+
+class Candidate:
+    def __init__(self, name, party):
+        self.name=name
+        self.party=party
+        self.votes = 0
+        print(f"[born c] Candidate: {self.name}, affiliated with party: {self.party}")
+
+
+    def __del__(self):
+        print(f"[killed c] Candidate {self.name} has been executed")
+
+    def receive_vote(self):
+        self.votes += 1
+        print(f"[vote +]A vote has been recorded for {self.name} affiliated with {self.party}. They have now {self.votes} vote(s).")
+
+class Ballot:
+    def __init__(self):
+        self.candidates = []
+
+    def add_candidate(self, candidate):
+        self.candidates.append(candidate)
+        print(f"[added to b] Candidate {candidate.name} has been added to the ballot.")
+
+    def record_vote(self, candidate, date):
+        if candidate in self.candidates:
+            print(f"[vote on record]A vote for {candidate.name} on {date} has been recorded.")
+            candidate.receive_vote()
+        else:
+            print(f"[err] no such candidate exists")
+
+    def __del__(self):
+        print("[burned ballot] Ballot has been destroyed.")
+
+class InPersonVoter(Voter):
+    def __init__(self, name, voter_id, poll_booth):
+        super().__init__(name, voter_id)
+        self.__poll_booth = poll_booth
+
+    def vote(self, voting_ballot, candidate, date):
+        print(f"[vote b] Voting at booth {self.__poll_booth} on {date}")
+        super().vote(voting_ballot, candidate, date)
+
+    def __del__(self):
+        print(f"[killed b] IRL voter {self.get_name()}, has been removed from the system.")
+
+
+class OnlineVoter(Voter):
+    def __init__(self, name, voter_id, ip_address):
+        super().__init__(name, voter_id)
+        self.__ip_address = ip_address
+
+    def vote(self, voting_ballot, candidate, date):
+        print(f"[vote o] Voting from ip: {self.__ip_address} on {date}")
+        super().vote(voting_ballot, candidate, date)
+
+    def __del__(self):
+        print(f"[killed o] Online voter {self.get_name()}, has been removed from the system.")
+
+c1 = Candidate("Adam Malysz", "Skoczek")
+c2 = Candidate("Ten inny", "Gorsza")
+
+ballot = Ballot()
+ballot.add_candidate(c1)
+ballot.add_candidate(c2)
+
+voter1 = OnlineVoter("Boron", 132,"172.0.0.0")
+voter2 = InPersonVoter("Defenestration", 133,"165")
+
+voter1.vote(ballot, c1, "1.06.2025")
+voter2.vote(ballot, c2, "1.06.2025")
+
+voter1.vote(ballot, c1, "1.06.2025")
+
